85c98dcfbd1e662be76a924fd6bff60fd990ef26
github username <github email address>
2019-09-18 19:25:09 -0500

    seeding data

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,18 @@
+# require_relative "./yelp_controller.rb" 
+
+# ? USER STORY
+# A user can see a list of all their lists - INDEX 
+# A user can delete a list
+# A user can create a list 
+
+
+class ListsController < ApplicationController
+  def index
+  end
+
+  def create 
+  end
+
+  def destroy 
+  end
+end 
