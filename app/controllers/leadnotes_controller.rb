34911be0b8a6a621e5d0d74e33967b2aadbed931
Grace Tan <gracetan37@gmail.com>
2019-09-26 13:57:36 -0500

    updated some stuff

--- app/controllers/leadnotes_controller.rb
+++ app/controllers/leadnotes_controller.rb
@@ -4,15 +4,22 @@ class LeadnotesController < ApplicationController
   end
 
   def create 
+   p params
+  #  leadnote = Leadnote.new(status: params[:status], next_steps: paramsuser_id: params[:user_id])
+    # if leadnote.save 
+    #   render json: leadnote, include: [:leads]
+    # else 
+    #   render json: {errors: leadnote.errors.full_messages}
+    # end
   end
 
   def show 
     p params
     leadnote = Leadnote.where(user_id: params[:user_id], lead_id: params[:lead_id])
     if leadnote
-      render json: leadnote, include: [:comment, :user, :lead]
+      render json: leadnote, include: [:comments]
     else 
-      render json: {errors: "No list found with that user id"}
+      render json: {errors: "No leadnote found with that user id"}
     end
   end
 
